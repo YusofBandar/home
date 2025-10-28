39d1a827ba872745dd304e7e301caef596cadac2
Yusof Bandar <ybandar1902@gmail.com>
2025-10-28 21:55:26 +0000

    Adds script to collect movies from letterboxd rss feed

---
+++ types/types.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  title: string;
+  link: string;
+  "letterboxd:filmTitle": string;
+  "letterboxd:watchedDate"?: string;
+  pubDate?: string;
+  "letterboxd:memberRating"?: string;
+}
